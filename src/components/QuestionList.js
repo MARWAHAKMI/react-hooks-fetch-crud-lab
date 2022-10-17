7f6371f1ce90881310cb9b7f92a9e554007fbe58
Marwa <marwahakmi@gmail.com>
2022-10-17 09:28:47 +0300

    assignment completed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import QuestionItem from "./QuestionItem";
+
+const fetchQuestions = async () => {
+  const response = await fetch("http://localhost:4000/questions");
+  const data = await response.json();
+  return data;
+};
 
 function QuestionList() {
+  const [questions, setQuestions] = React.useState([]);
+
+  useEffect(() => {
+    fetchQuestions().then((data) => setQuestions(data));
+  }, []);
+
+  function handleDeleteQuestion(id) {
+    const updatedQuestions = questions.filter((question) => question.id !== id);
+    setQuestions(updatedQuestions);
+  }
+
+  function handleAddQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion]);
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onAddAnswer={handleAddQuestion}
+            onDeleteQuestion={handleDeleteQuestion}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
